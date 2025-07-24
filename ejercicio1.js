3b7e466d8a38f00a300c658905611673e9650d10
lucy <lucyhapsti@gmail.com>
2025-07-24 21:35:17 +0200

    first two

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+var miSuperheroeFavorito = "Hulk";
+var numeroFavorito = 50;
+
+var altura = 5;
+var longitud = 10;
+
+var suma = altura + longitud;
+
+console.log(miSuperheroeFavorito, numeroFavorito, suma);
---
+++ ejercicio2.js
@@ -0,0 +1,35 @@
+//1
+
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+jedi.edad = 25;
+console.log(jedi.edad);
+
+//2
+
+var nombre = "Leia";
+var apellido = "Organa";
+var edad = 20;
+
+console.log("Soy " + nombre + " " + apellido + ", tengo " + edad + " años y soy una princesa de Alderaan.");
+
+//3
+
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+var total = sable1.precio + sable2.precio;
+console.log(total)
+
+//4
+
+let precioBaseGlobal = 10000;
+
+const nave1 = {nombre: "Ala-X", precioBase: 50000, precioFinal: 60000};
+const nave2 = {nombre: "Halcón Milenario", precioBase: 70000, precioFinal: 80000};
+
+precioBaseGlobal = 25000;
+
+nave1.precioFinal = precioBaseGlobal + nave1.precioBase;
+nave2.precioFinal = precioBaseGlobal + nave2.precioBase;
+
+console.log(nave1.precioFinal, nave2.precioFinal)
